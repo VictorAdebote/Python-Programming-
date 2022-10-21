5533bed28d8fd5b6698c8463417e254c93bdd068
VictorAdebote <116380779+VictorAdebote@users.noreply.github.com>
2022-10-21 18:58:23 +0100

    Add files via upload

---
+++ Month1.py
@@ -0,0 +1,40 @@
+'''
+ practical assessment
+'''
+
+# variables
+message='victor is my name'
+first_number=5
+y=5.9
+z=True
+a=8
+
+# output
+print(message)
+print(first_number)
+print(y)
+print(z)
+print(a)
+
+# types
+print(type(message))
+print(type(first_number))
+print(type(y))
+print(type(z))
+print(type(a))
+
+# methods and placeholder
+first_name='victor'
+last_name='adebote'
+full_name= f'{first_name} {last_name}'
+message2= f'Hello, {full_name.title()}'
+print(f'Hello my name is {first_name} {last_name}')
+
+# arithmetic operations
+print(7 * 6)
+print(100 + 100)
+print(45 / 9)
+print(67 - 58)
+print(78 % 78)
+print(45 // 6)
+
