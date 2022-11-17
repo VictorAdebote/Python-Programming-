df5e05d19c0a821078abf566520b81137a17ca04
VictorAdebote <116380779+VictorAdebote@users.noreply.github.com>
2022-11-17 16:35:23 +0000

    Add files via upload

---
+++ Band name generator.py
@@ -0,0 +1,8 @@
+# input
+print('Welcome to Band name generator')
+print()
+colour=input('What is your favourite colour? ')
+animal=input('What is your favourite animal? ')
+print()
+print(f'Your band name is \n{colour.upper()}{animal.upper()}')
+
