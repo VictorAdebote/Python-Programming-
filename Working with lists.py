fc313648cec7e65352bbc33b8176f8f292c62690
VictorAdebote <116380779+VictorAdebote@users.noreply.github.com>
2022-12-09 17:36:55 +0000

    Add files via upload

---
+++ Working with lists.py
@@ -0,0 +1,16 @@
+names=['Angela', 'Victor', 'Denzel', 'Olivia', 'Daniel', 'Vincent', 'Max', 'Robert', 'Nathan', 'Emily']
+for name in names:
+    print(name)
+    print(f'{name} you\'r invited to my party.')
+    print(name.upper())
+    print(name.lower())
+print()
+pizzas=['Peroni pizza', 'Mushrooms', 'Anchovies']
+for pizza in pizzas:
+    print(f'I like {pizza}.')
+print(f'I really like {pizzas[0]}.')
+print()
+animals_common_characteristics=['Dog', 'birds', 'Toad', 'Horse']
+for animals in animals_common_characteristics:
+    print(f'A {animals} knows when natural disasters happens.')
+print(f'A {animals_common_characteristics[3]} would make a great companion.')
