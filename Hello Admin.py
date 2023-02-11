b5bb072269a556ef38bb70038eadd4bcd2f043c7
VictorAdebote <116380779+VictorAdebote@users.noreply.github.com>
2023-02-11 20:25:26 +0000

    Add files via upload

---
+++ Hello Admin.py
@@ -0,0 +1,14 @@
+Hello_admin = ['Hayden', 'Jayden', 'Mia', 'Olivia', 'admin ']
+print('Welcome to Hello Admin')
+print()
+if 'Hayden' in Hello_admin:
+    print(f'Hello {Hello_admin[0]} thank you for logging in again')
+print()
+if 'Jayden' in Hello_admin:
+    print(f'Welcome {Hello_admin[1]} pleased to welcome you again')
+print()
+if 'Mia' in Hello_admin:
+    print(f'Welcome {Hello_admin[2]}')
+print()
+if 'Olivia' in Hello_admin:
+    print(f'Welcome {Hello_admin[3]}')
