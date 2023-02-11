a6eec4838ae8689c621a3c0b83ce2a4a4647d1b2
VictorAdebote <116380779+VictorAdebote@users.noreply.github.com>
2023-02-11 19:44:49 +0000

    Add files via upload

---
+++ imagine an alien.py
@@ -0,0 +1,7 @@
+alien_color = input('green, yellow, red: ')
+if alien_color == 'green':
+    print('The player has earns 5 points. ')
+elif alien_color == 'yellow':
+    print('The player has earned 10 points. ')
+elif alien_color == 'red':
+    print('Player has earned 15 points. ')
